a816d3df30e9505b7df9f537c42805b7fa20baf2
Orest Dubay <orest3.dubay@gmail.com>
2019-06-04 22:35:22 +0200

    initail matplotlib command

---
+++ liquer/ext/lq_matplotlib.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import io
+from urllib.request import urlopen
+from matplotlib.figure import Figure
+from matplotlib import pyplot as plt
+import numpy as np
+from liquer.commands import command
+
+
+@command
+def MPL(state, *series):
+    """Matplotlib chart
+    """
+    fig = plt.figure(figsize=(8, 6), dpi=300)
+    axis = fig.add_subplot(1, 1, 1)
+    series = list(reversed(list(series)))
+    df = state.get()
+    
+    while len(series):
+        t = series.pop()
+        if t in ["jpg","png","svg"]:
+            extension = t
+            continue
+        elif t == "xy":
+            xcol = state.expand_column(series.pop())
+            ycol = state.expand_column(series.pop())
+            state.log_info(f"Chart XY ({xcol} {ycol})")
+            axis.plot(df[xcol],df[ycol],label=state.column_label(ycol))
+            continue
+        elif t == "xye":
+            xcol = state.expand_column(series.pop())
+            ycol = state.expand_column(series.pop())
+            ecol = state.expand_column(series.pop())
+            state.log_info(f"Chart XY ({xcol} {ycol}) Error:{ecol}")
+            axis.errorbar(df[xcol],df[ycol],yerr=df[ecol],label=state.column_label(ycol))
+            continue
+        elif t == "xyee":
+            xcol = state.expand_column(series.pop())
+            ycol = state.expand_column(series.pop())
+            e1col = state.expand_column(series.pop())
+            e2col = state.expand_column(series.pop())
+            state.log_info(f"Chart XY ({xcol} {ycol}) Error:({e1col},{e2col})")
+            axis.errorbar(df[xcol],df[ycol],yerr=[df[e1col],df[e2col]],label=state.column_label(ycol))
+            continue
+        elif t == "cxy":
+            c = series.pop()
+            xcol = state.expand_column(series.pop())
+            ycol = state.expand_column(series.pop())
+            axis.plot(df[xcol],df[ycol],c,label=state.column_label(ycol))
+            continue
+        else:
+            state.log_warning(f"Unrecognized MPL parameter {t}")
+    fig.legend()
+    output = io.BytesIO()
+    fig.savefig(output, dpi=300, format=extension)
+    return state.with_data(output.getvalue()).with_filename(f"image.{extension}")
