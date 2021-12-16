c198f06a9ec70f201cda51d9602c339457c80212
Orest Dubay <orest3.dubay@gmail.com>
2021-12-16 23:46:39 +0100

    Tornado handlers updated

--- liquer/server/tornado_handlers.py
+++ liquer/server/tornado_handlers.py
@@ -3,36 +3,118 @@ import tornado.web
 import liquer.server.handlers as h
 
 
+def liquer_static_path():
+    import liquer.server
+    import os.path
+
+    return os.path.join(os.path.dirname(liquer.server.__file__), "static")
+
+
 class LiquerIndexHandler(h.LiquerIndexHandler, tornado.web.RequestHandler):
     pass
 
-
 class LiquerJsHandler(h.LiquerJsHandler, tornado.web.RequestHandler):
     pass
-
 
 # /api/commands.json
 class CommandsHandler(h.CommandsHandler, tornado.web.RequestHandler):
     pass
 
-
-# /api/debug-json/<path:query>
-class DebugQueryHandler(h.DebugQueryHandler, tornado.web.RequestHandler):
+#'/submit/<query>
+class SubmitHandler(h.SubmitHandler, tornado.web.RequestHandler):
     pass
-
 
 # /q/<path:query>
 class QueryHandler(h.QueryHandler, tornado.web.RequestHandler):
     pass
 
+#/api/cache/get/<path:query>
+class CacheGetDataHandler(h.CacheGetDataHandler, tornado.web.RequestHandler):
+    pass
 
+#/api/cache/meta/<path:query>
+class CacheMetadataHandler(h.CacheMetadataHandler, tornado.web.RequestHandler):
+    pass
+
+#/api/cache/remove/<path:query>
+class CacheRemoveHandler(h.CacheRemoveHandler, tornado.web.RequestHandler):
+    pass
+
+#/api/cache/contains/<path:query>
+class CacheContainsHandler(h.CacheContainsHandler, tornado.web.RequestHandler):
+    pass
+
+#/api/cache/keys.json
+class CacheKeysHandler(h.CacheKeysHandler, tornado.web.RequestHandler):
+    pass
+
+#/api/cache/clean
+class CacheCleanHandler(h.CacheCleanHandler, tornado.web.RequestHandler):
+    pass
+
+#/api/commands.json
+class CommandsHandler(h.CommandsHandler, tornado.web.RequestHandler):
+    pass
+
+#/api/debug-json/<path:query>
+class GetMetadataHandler(h.GetMetadataHandler, tornado.web.RequestHandler):
+    pass
+
+#/api/stored_metadata/<path:query>
+class GetStoredMetadataHandler(h.GetStoredMetadataHandler, tornado.web.RequestHandler):
+    pass
+
+# /api/build
 class BuildHandler(h.BuildHandler, tornado.web.RequestHandler):
     pass
 
-
+#'/api/register_command/
 class RegisterCommandHandler(h.RegisterCommandHandler, tornado.web.RequestHandler):
     pass
 
+#/api/store/data/<path:query>
+class StoreDataHandler(h.StoreDataHandler, tornado.web.RequestHandler):
+    pass
+
+#/api/store/metadata/<path:query>
+class StoreMetadataHandler(h.StoreMetadataHandler, tornado.web.RequestHandler):
+    pass
+
+#/web/<path:query>
+class WebStoreHandler(h.WebStoreHandler, tornado.web.RequestHandler):
+    pass
+
+#/api/store/remove/<path:query>
+class StoreRemoveHandler(h.StoreRemoveHandler, tornado.web.RequestHandler):
+    pass
+
+#/api/store/removedir/<path:query>
+class StoreRemovedirHandler(h.StoreRemovedirHandler, tornado.web.RequestHandler):
+    pass
+
+#/api/store/contains/<path:query>
+class StoreContainsHandler(h.StoreContainsHandler, tornado.web.RequestHandler):
+    pass
+
+#/api/store/is_dir/<path:query>
+class StoreIsDirHandler(h.StoreIsDirHandler, tornado.web.RequestHandler):
+    pass
+
+#/api/store/keys")
+class StoreKeysHandler(h.StoreKeysHandler, tornado.web.RequestHandler):
+    pass
+
+#/api/store/listdir/<path:query>
+class StoreListdirHandler(h.StoreListdirHandler, tornado.web.RequestHandler):
+    pass
+
+#/api/store/makedir/<path:query>
+class StoreMakedirHandler(h.StoreMakedirHandler, tornado.web.RequestHandler):
+    pass
+
+
+#class DebugQueryHandler(h.DebugQueryHandler, tornado.web.RequestHandler):
+#    pass
 
 if __name__ == "__main__":
     import liquer.ext.basic
@@ -55,10 +137,30 @@ if __name__ == "__main__":
     application = tornado.web.Application(
         [
             (r"/liquer/api/commands.json", CommandsHandler),
-            (r"/liquer/api/debug-json/(.*)", DebugQueryHandler),
-            (r"/liquer/api/build", BuildHandler),
-            (r"/liquer/api/register_command/(.*)", RegisterCommandHandler),
+            (r"/liquer/submit/(.*)", SubmitHandler),
             (r"/liquer/q/(.*)", QueryHandler),
+            (r"/liquer/api/cache/get/(.*)", CacheGetDataHandler),
+            (r"/liquer/api/cache/meta/(.*)", CacheMetadataHandler),
+            (r"/liquer/api/cache/remove/(.*)", CacheRemoveHandler),
+            (r"/liquer/api/cache/contains/(.*)", CacheContainsHandler),
+            (r"/liquer/api/cache/keys.json", CacheKeysHandler),
+            (r"/liquer/api/cache/clean", CacheCleanHandler),
+            (r"/liquer/api/commands.json", CommandsHandler),
+            (r"/liquer/api/debug-json/(.*)", GetMetadataHandler),
+            (r"/liquer/api/metadata/(.*)", GetMetadataHandler),
+            (r"/liquer/api/stored_metadata/(.*)", GetStoredMetadataHandler),
+            (r"/liquer/api/build", BuildHandler),
+            (r"/liquer/api/register_command", RegisterCommandHandler),
+            (r"/liquer/api/store/data/(.*)", StoreDataHandler),
+            (r"/liquer/api/store/metadata/(.*)", StoreMetadataHandler),
+            (r"/liquer/web/(.*)", WebStoreHandler),
+            (r"/liquer/api/store/remove/(.*)", StoreRemoveHandler),
+            (r"/liquer/api/store/removedir/(.*)", StoreRemovedirHandler),
+            (r"/liquer/api/store/contains/(.*)", StoreContainsHandler),
+            (r"/liquer/api/store/is_dir/(.*)", StoreIsDirHandler),
+            (r"/liquer/api/store/keys", StoreKeysHandler),
+            (r"/liquer/api/store/listdir/(.*)", StoreListdirHandler),
+            (r"/liquer/api/store/makedir/(.*)", StoreMakedirHandler),
             (
                 r"/static/(.*)",
                 tornado.web.StaticFileHandler,
