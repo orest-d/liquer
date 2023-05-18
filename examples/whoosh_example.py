18067267cc8e0843d055089e229313cc98a5b7f6
Orest Dubay <orest3.dubay@gmail.com>
2023-05-18 20:30:51 +0200

    whoosh starting to work

---
+++ examples/whoosh_example.py
@@ -0,0 +1,74 @@
+# Make it run from the examples directory
+import sys
+sys.path.append("..")
+
+from liquer import *
+from liquer.store import web_mount, MemoryStore, get_store
+import liquer.ext.meta
+import liquer.ext.lq_pandas
+import liquer.ext.lq_whoosh as whs
+
+
+### Create Flask app and register LiQuer blueprint
+from flask import Flask
+import liquer.server.blueprint as bp
+app = Flask(__name__)
+app.register_blueprint(bp.app, url_prefix='/liquer')
+
+# Mount a new store into the web/hello. This will be just in memory (MemoryStore).
+web_mount("hello",MemoryStore())
+
+# Initialize whoosh with a temporary directory
+import tempfile
+temp_dir = tempfile.mkdtemp()
+whs.init(temp_dir)
+
+# Create some files in the web store 
+get_store().store("web/hello/index.html", "Hello from web store!", {"title":"Hello document"})
+# Create some files in the web store 
+get_store().store("web/hello/another.html", "Another document", {"title":"Another document"})
+
+@command
+def display(txt):
+    if type(txt) == bytes: # Data from store come as bytes, so they need to be decoded
+        txt = txt.decode("utf-8")
+    return f"""<html>
+<body>
+<h1>Display</h1>
+<pre>{txt}</pre>
+</body>
+</html>"""
+
+@app.route('/')
+@app.route('/index.html')
+def index():
+    return f"""<h1>Hello Whoosh</h1>
+    Index is in {temp_dir}
+    <hr/>
+    <ul>
+    <li><a href="/liquer/q/ns-whoosh/search-document/result.json">Search 'document' (json)</a></li>
+    <li><a href="/liquer/q/ns-whoosh/search-document/to_html/result.html">Search 'document' (html)</a></li>
+    <li><a href="/liquer/q/ns-whoosh/search-another/result.json">Search 'another' (json)</a></li>
+    <li><a href="/liquer/q/ns-whoosh/search-another/to_html/result.html">Search 'another' (html)</a></li>
+    <li><a href="/liquer/q/ns-whoosh/reindex_store">reindex store</a></li>
+    </ul>
+    <br/>
+    <hr/>
+    <ul>
+    <li><a href="/liquer/web/hello/index.html">Shortcut to web store</a></li>
+    <li><a href="/liquer/web/hello">This works too</a></li>
+    <li><a href="/liquer/api/store/data/web/hello/index.html">Store API</a></li>
+    <li><a href="/liquer/api/store/metadata/web/hello/index.html">Store API (metadata)</a></li>
+    <li><a href="/liquer/q/web/hello/index.html/-/display/index.html">Query with display command</a></li>
+    <li><a href="/liquer/q/-R-meta/web/hello/index.html/-/ns-meta/status_md/status.txt">Status of the file in store</a></li>
+    <li><a href="/liquer/q/web/hello/index.html/-/display/ns-meta/state/status_md/status.txt">Display status</a></li>
+    <li><a href="/liquer/q/-R-meta/web/hello/-/ns-meta/status_md/status.txt">Dir status</a></li>
+    <li><a href="/liquer/q/-R-meta/web/hello/-/ns-meta/dir_status/status.json">Dir status (json)</a></li>
+    <li><a href="/liquer/q/-R-meta/web/hello/-/ns-meta/dir_status_df/status.html">Dir status (html)</a></li>
+    <li><a href="/liquer/api/store/upload/web/hello/test.txt">Upload test.txt</a></li>
+    <li><a href="/liquer/web/hello/test.txt">test.txt</a></li>
+    </ul>
+    """
+
+if __name__ == '__main__':
+    app.run()
---
+++ liquer/ext/lq_whoosh.py
@@ -0,0 +1,133 @@
+import json
+from whoosh.index import create_in
+from whoosh.fields import *
+from liquer import *
+from liquer.web import key_link
+from liquer.indexer import Indexer, register_indexer
+from whoosh.qparser import QueryParser, MultifieldParser
+from whoosh.query import Every
+
+_schema = None
+_ix = None
+
+
+def init(dir="whoosh_index"):
+    """Initialize the Whoosh search engine."""
+    global _schema, _ix, _writer
+    _schema = Schema(
+        title=TEXT(stored=True),
+        description=TEXT(stored=True),
+        characteristics=TEXT(stored=True),
+        key=ID(unique=True, stored=True),
+        text_key=TEXT(stored=False),
+    )
+    _ix = create_in(dir, _schema)
+    register_indexer(AddToWhooshIndex())
+
+
+def get_writer():
+    global _ix
+    if _ix is None:
+        init()
+    return _ix.writer()
+
+
+class AddToWhooshIndex(Indexer):
+    HIGH_PRIORITY = 10
+    NORMAL_PRIORITY = 100
+    LOW_PRIORITY = 1000
+
+    def identifier(self):
+        return f"AddToWhooshIndex()"
+
+    def __call__(self, key=None, query=None, data=None, metadata=None):
+        if metadata is None or key is None:
+            return metadata
+        add(
+            metadata.get("title", ""),
+            metadata.get("description", ""),
+            metadata.get("characteristics", ""),
+            key=key,
+        )
+        return metadata
+
+
+def add(title, description, characteristics, key, commit=True, writer=None):
+    if writer is None:
+        writer = get_writer()
+
+    writer.update_document(
+        title=title,
+        description=description,
+        characteristics=characteristics,
+        key=key,
+        text_key=key,
+    )
+    if commit:
+        writer.commit()
+
+
+@first_command(ns="whoosh")
+def reindex_store(context=None):
+    context = get_context(context)
+
+    with get_writer() as writer:
+        store = context.store()
+        for key in store.keys():
+            metadata = store.get_metadata(key)
+            print(f"Adding {key} to index")
+            add(
+                title=metadata.get("title", ""),
+                description=metadata.get("description", ""),
+                characteristics=metadata.get("characteristics", ""),
+                key=key,
+                commit=False,
+                writer=writer,
+            )
+        # writer.commit()
+
+
+@first_command(ns="whoosh")
+def search(query, limit=100, context=None):
+    context = get_context(context)
+    with _ix.searcher() as searcher:
+        q = MultifieldParser(["title", "description", "key", "text_key"], _schema).parse(query)
+        results = searcher.search(q)
+        r = []
+        for i, x in enumerate(results):
+            r.append(dict(key=x["key"], title=x["title"], description=x["description"]))
+            if i > limit:
+                break
+        return r
+
+
+@first_command(ns="whoosh")
+def search_json(query, limit=100, context=None):
+    context = get_context(context)
+    context.mimetype = "text/json"
+    return json.dumps(search(query, limit, context=context))
+
+
+@command(ns="whoosh")
+def to_html(search_result, context=None):
+    context = get_context(context)
+    text = """
+<html>
+  <body>
+    <table>
+    """
+    for r in search_result:
+        link = key_link(r["key"], path_only=True)
+        text += f"""
+      <tr>
+        <td><a href="{link}">{r['key']}</a></td>
+        <td><a href="{link}">{r['title']}</a></td>
+        <td>{r['description']}</td>
+      </tr>
+        """
+    text += """
+    </table>
+    </body>
+</html>
+    """
+    return text
